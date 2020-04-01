6e13412cc6abecc14e0da3260ed7bb4472ecd077
Миша Шмелёв <cryptecker@hotmail.com>
2020-04-01 15:36:06 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadLine();
+        }
+    }
+}
